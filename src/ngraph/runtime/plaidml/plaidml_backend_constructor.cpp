409e219131f24b7138621fe084d2a87d7c0495ac
Conrad Jones <cjones@turboit.co.uk>
2020-04-16 08:22:09 +0100

    Test

--- src/ngraph/runtime/plaidml/plaidml_backend_constructor.cpp
+++ src/ngraph/runtime/plaidml/plaidml_backend_constructor.cpp
@@ -22,6 +22,9 @@
 extern "C" PLAIDML_BACKEND_API void ngraph_register_plaidml_backend()
 {
     ngraph::runtime::BackendManager::register_backend("PlaidML", [](const std::string& config) {
+        
+        std::cout << "Backend called\n";
+        
         return std::make_shared<ngraph::runtime::plaidml::PlaidML_Backend>(config);
     });
 }
